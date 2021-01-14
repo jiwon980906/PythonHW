b97bda9a3a4d974c97b46a441edae58c2f744fa1
jiwon980906 <77109918+jiwon980906@users.noreply.github.com>
2021-01-14 23:43:22 +0900

    Unit26
    퀴즈 및 심사문제

---
+++ Unit26 Quiz1-2.py
@@ -0,0 +1,8 @@
+a = {1,2,3}
+b = {3,4,5}
+
+print(set.union(a,b))
+print(a^b)
+print(a-b)
+print(a&b)
+print(set.difference(b,a))
---
+++ Unit26 Quiz1-6.py
@@ -0,0 +1,2 @@
+print({i for i in range(10) if i is not 2 and i is not 7})
+print({i for i in range(10) if i != 2 and i != 7})
