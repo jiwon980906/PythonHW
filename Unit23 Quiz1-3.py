0e46cf2920fa409827008fab8cc2c43414154adf
jiwon980906 <77109918+jiwon980906@users.noreply.github.com>
2021-01-14 23:42:14 +0900

    Unit23
    퀴즈 및 심사문제

---
+++ Unit23 Quiz1-3.py
@@ -0,0 +1,4 @@
+a =[[20], [30], [40]]
+for i in a:
+    for j in i:
+        print(j)
---
+++ Unit23 Quiz1-5.py
@@ -0,0 +1,5 @@
+a = [[0 for j in range(3)] for i in range(3)]
+print(a)
+
+c = [[0,0,0] for i in range(3)]
+print(c)
